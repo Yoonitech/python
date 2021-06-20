163f9352be917ef32bb933d0248fc43685fc153e
P-unes-AIs-GOD <85844438+P-unes-AIs-GOD@users.noreply.github.com>
2021-06-20 04:50:41 -0700

    Add files via upload
    this code separate numbers from row input of numbers

---
+++ rawintinput.py
@@ -0,0 +1,23 @@
+wi=input()
+list_wi=[]
+l=len(wi)
+k=-1
+for i in range(0,l):
+    if i > k and wi[i] != ' ' :
+        sum = int(wi[i])
+        t=1
+        if  i+t != l :
+            while wi[i+t] != ' ' :
+                
+                if  i+t != l :
+                    sum = (sum*10) + int(wi[i+t])
+                k=t+i
+             
+                if i+t+1 != l :
+                    t=t+1
+                else:
+                    break    
+                        
+                
+        list_wi.append(sum)
+print(list_wi)        
